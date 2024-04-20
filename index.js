c1c8ba853b7e55225005c9109dbc49d26cfe9b76
tranthang <tranthangusername@gmail.com>
2024-04-20 15:46:17 +0700

    update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/create-payment-link', async (req, res) => {
     res.redirect(303, paymentLink.checkoutUrl);
 })
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/abc", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
