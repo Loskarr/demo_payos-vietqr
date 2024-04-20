f3429d8f63967d2037007938b2d8845f1d8f3aa7
tranthang <tranthangusername@gmail.com>
2024-04-20 18:29:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.post('/create-payment-link', async (req, res) => {
         amount: 1000,
         description :'Thanh toan mi tom',
         orderCode: Math.floor(Math.random() * 9007199254740990) + 1,
-        returnUrl: `https://drive.google.com/file/d/1DaoW9CH7ri29mHZ5Qtxl6uMo-wH3X4ol/view`,
+        returnUrl: `https://demo-casso-46x2vxwaj-loskarrs-projects.vercel.app/success.html`,
         //cancelUrl: `https://demo-casso-g36v549e8-loskarrs-projects.vercel.app/cancel.html`
-        cancelUrl: `https://drive.google.com/file/d/1DaoW9CH7ri29mHZ5Qtxl6uMo-wH3X4ol/view`
+        cancelUrl: `https://demo-casso-46x2vxwaj-loskarrs-projects.vercel.app/cancel.html`
     }
     const paymentLink = await payos.createPaymentLink(order);
     res.redirect(303, paymentLink.checkoutUrl);
